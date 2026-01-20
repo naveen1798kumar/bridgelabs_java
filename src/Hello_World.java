46c2275905586519771752f9436dbe91a651f483
naveen1798kumar <naveennaveennk1798@gmail.com>
2026-01-20 12:18:26 +0530

    first commit

---
+++ src/Hello_World.java
@@ -0,0 +1,9 @@
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Im Naveen");
+
+        short val = 1000;
+        char txt = 'N';
+        System.out.println(txt);
+    }
+}
