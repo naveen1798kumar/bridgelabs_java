83ffa7288ae181a8d7c01405ada42ebeb282c351
naveen1798kumar <naveennaveennk1798@gmail.com>
2026-01-20 16:04:52 +0530

    added tasks

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -5,5 +5,14 @@ public class Hello_World {
         short val = 1000;
         char txt = 'N';
         System.out.println(txt);
+
+        System.out.println("Hello");
     }
 }
+
+
+//public class Employee_Data{
+//    public static void main(String[] args) {
+//        System.out.println("Hello");
+//    }
+//}
